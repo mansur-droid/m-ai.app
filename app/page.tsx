8d9552a48a9d2369781ce3dd16f8cc5f82cc6b44
mansur-droid <mansur@adlift.agency>
2026-07-30 18:23:30 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+export default function Home(){
+ return <main style={{display:'grid',placeItems:'center',height:'100vh',background:'#09090b',color:'white'}}>
+ <div><h1>m.ai</h1><p>Your Personal AI Operating System</p></div></main>
+}
